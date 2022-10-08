e7bbc82fcf1fa05cc53a8847bf99287ad4d129e8
Fernando Candela <20171937@aloe.ulima.edu.pe>
2022-10-07 22:09:37 -0500

    add nav menu

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
     NavMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     NavMenuComponent
