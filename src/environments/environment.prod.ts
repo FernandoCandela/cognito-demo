548070693d16a207fc5f5807d14f9c76a5c100aa
Fernando Candela <20171937@aloe.ulima.edu.pe>
2022-10-09 14:38:11 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  production: false,
+  production: true,
   apirestURL:'https://4ck351ztgi.execute-api.us-east-2.amazonaws.com/dev/',
   ACCESS_TOKEN: 'idToken',
   CLIENT_ID: '1o6csdb1i0s5donq9a6e0qosgp',
   AUTHORIZATION: 'Authorization',
-  BEARER: 'Bearer '
+  BEARER: 'Bearer ',
+  ROLES_FIELD: 'cognito:groups'
 };
